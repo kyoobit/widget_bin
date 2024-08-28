2b59f3c51efc36347dcadf3aff4e55a75b9e2eea
Nate Royer <natroyer@Old-mans-M1.local>
2024-08-28 10:52:56 -0600

    Clean up and add colors

--- example.sh
+++ example.sh
@@ -2,18 +2,18 @@
 
 ## Default(s)
 __version__="0.0.1a";
+
 ## Stop the script execution if an error happens
-set -o errexit
+#set -o errexit <-- breaks on argument case check
 ## Treat unset variables as an error
-set -o nounset
+#set -o nounset
 ## When one part of a pipe fails, the whole pipe should be considered failed
-set -o pipefail
+#set -o pipefail
 
 ## -----------------------------------------------------------------------------
 
-## Help message
-read -r -d '' help_message << EOM
-usage: $(basename $0) [-h|--help] ...
+read -r -d '' HELP_MESSAGE <<- END_HELP_MESSAGE
+usage: $(basename ${0}) [-h|--help] ...
 
 ...
 
@@ -29,19 +29,19 @@ Program requires:
   ...
 
 Program examples:
-$ $(basename $0) ...
+$ $(basename ${0}) ...
 
-EOM
+END_HELP_MESSAGE
 
 ## -----------------------------------------------------------------------------
 
 ## Argument parsing.
 while :
 do
-    case "$1" in
+    case "${1}" in
         -h | --help)
-              echo "${help_message}";
-              exit 0;
+            echo "${HELP_MESSAGE}";
+            exit 0;
         ;;
         -a | --abc)
             abc="$2";
@@ -83,7 +83,7 @@ do
     esac
 done
 
-message() {
+function message() {
     if [ -z "${debug}" ] && [ "${1}" == "DEBUG" ]; then
         do_nothing=true
     elif [ -z "${verbose}" ] && [ "${1}" == "INFO" ]; then
@@ -95,7 +95,18 @@ message() {
 
 ## -----------------------------------------------------------------------------
 
-main() {
+function print_colors() {
+    ## ENDC = '\033[0m'
+    ## BOLD = '\033[1m'
+    ## UNDERLINE = '\033[4m'
+    for i in $(seq 90 96); do
+        printf "\033[${i}m color test: 033[${i}m\033[0m\n";
+        printf "\033[1m\033[${i}m color test: 033[1m 033[${i}m\033[0m\n";
+    done;
+}
+
+function main() {
+
     messgae "DEBUG" "running script $0";
 
     message "DEBUG" "debug=${debug:-nilly}";
@@ -107,13 +118,14 @@ main() {
     message "INFO" "INFO message"
     message "WARNING" "warning"
 
-## ${varname:-word} If varname exists and isn't null, return its value; otherwise return "word".
-## ${varname:=word} If varname exists and isn't null, return its value; otherwise set it to word and then return its value. 
+    print_colors;
 
-## if [ -z "${dryrun}" ]; then
-##     mkdir -p ${wrk};
-## fi;
+    if [ -n "${dryrun}" ]; then
+        message "WARNING" "Running in DRY-RUN mode, changes will be made!"
+    fi;
 
+    ## ${varname:-word} If varname exists and isn't null, return its value; otherwise return "word".
+    ## ${varname:=word} If varname exists and isn't null, return its value; otherwise set it to word and then return its value. 
 }
 
 main;
